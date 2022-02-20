daf9eb0c32855dca868251cafcf4dca5b496433b
zouxin <zouxin78@qq.com>
2022-02-20 15:12:56 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @Author: Deng
+ * @Date: 2022/2/20 15:07
+ */
+public class GitTest {
+  public static void main(String[] args) {
+    System.out.println("hello git!");
+  }
+}
